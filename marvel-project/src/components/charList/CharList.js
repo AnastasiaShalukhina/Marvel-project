4774fced33011c4a54916036e3cddb2c1aa95856
AnastasiaShalukhina <exachka4@gmail.com>
2023-04-18 12:51:04 +0300

    add refs to charList and change the structure of component

--- marvel-project/src/components/charList/CharList.js
+++ marvel-project/src/components/charList/CharList.js
@@ -62,18 +62,69 @@ class CharList extends Component {
     })
   }
 
-  render() {
-    const { charList, loading, error, offset, newItemLoading, charsEnded } = this.state;
-    const onCharSelected = this.props.onCharSelected;
-    const spinner = loading ? <Spinner /> : null;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const view = !(loading || error) ? < View props={{ charList, onCharSelected }} /> : null;
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  }
+
+  focusOnItem = (id) => {
+    this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+    this.itemRefs[id].classList.add('char__item_selected');
+    this.itemRefs[id].focus();
+  }
+
+  renderItems(arr) {
+    const items = arr.map((item, i) => {
+      let imgStyle = { 'objectFit': 'cover' };
+      if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = { 'objectFit': 'unset' };
+      }
+
+      return (
+        <li
+          className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
+          key={item.id}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === ' ' || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}>
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className="char__name">{item.name}</div>
+        </li>
+      )
+    });
 
     return (
-      <div className='charr__list'>
-        {spinner}
+      <ul className="char__grid">
+        {items}
+      </ul>
+    )
+  }
+
+  render() {
+
+    const { charList, loading, error, offset, newItemLoading, charsEnded } = this.state;
+
+    const items = this.renderItems(charList);
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
+
+    return (
+      <div className="char__list">
         {errorMessage}
-        {view}
+        {spinner}
+        {content}
         <button
           className="button button__main button__long"
           disabled={newItemLoading}
@@ -83,38 +134,11 @@ class CharList extends Component {
         </button>
       </div>
     )
-  };
-}
-
-const View = ({ props }) => {
-  const { charList, onCharSelected } = props;
-  const items = charList.map((char) => {
-    const thumbnailArr = char.thumbnail.split('/');
-    const endOfThumnail = thumbnailArr[thumbnailArr.length - 1];
-    const imageNotFound = endOfThumnail === "image_not_available.jpg" ? true : false;
-    const imageStyles = imageNotFound ? { objectFit: "contain" } : { objectFit: "cover" };
-
-    return (
-      <li
-        className="char__item"
-        key={char.id}
-        onClick={() => onCharSelected(char.id)}
-      >
-        <img style={imageStyles} src={char.thumbnail} alt={char.name} />
-        <div className="char__name">{char.name}</div>
-      </li>
-    )
-  })
-
-  return (
-    <ul className="char__grid">
-      {items}
-    </ul>
-  )
+  }
 }
 
 CharList.propTypes = {
-  onCharSelected: PropTypes.func
+  onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
